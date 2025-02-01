a9439417e8bbed0c7fd0251edb920aabe6733d35
simenforris <simenforris@gmail.com>
2025-02-01 16:10:48 +0100

    refactor: new layout for home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    responsiveImages: true,
+    svg: true,
+  },
+  server: {
+    host: true,
+  },
+});
